2d87f83726db3912a10269c70031a32a6db28f54
Markus Härkönen <markus.harkonen@tuni.fi>
2025-01-29 21:10:48 +0200

    Changed localhost to service4 for tests

--- tests/test_api.sh
+++ tests/test_api.sh
@@ -1,19 +1,9 @@
 # Variables
-url='localhost:8197/api/state'
+url='service4:8197/api/state'
 tests_passed=1
-
-# Debug: Test Google curl
-debug_response=$(curl --write-out "%{http_code}" --silent --output /dev/null "https://www.google.com")
-
-# Debug:Print Google response
-echo "Response from https://www.google.com: $debug_response"
-
 
 # Test calling the /state REST API function with GET method at port 8197
 response=$(curl --write-out "%{http_code}" --silent --output /dev/null "$url")
-
-# Debugging: Print response
-echo "Response: $response"
 
 # Checking if the response code is 200
 if [ "$response" == '200' ]; then
@@ -22,8 +12,5 @@ if [ "$response" == '200' ]; then
 else
     echo "Test failed: Expected 200, but got $response"
 fi
-
-# Debugging: Print tests_passed
-echo "Tests passed: $tests_passed"
 
 exit $tests_passed
